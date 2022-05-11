40f00258fc72da6497d168229764537e4c10d3e7
jongmin <ace4463@gmail.com>
2022-05-11 17:48:15 +0900

    Add MyInflater

---
+++ MyInflater/app/src/main/java/org/tech/town/inflater/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.tech.town.inflater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            layoutInflater.inflate(R.layout.part1, container, true)
+        }
+    }
+}
