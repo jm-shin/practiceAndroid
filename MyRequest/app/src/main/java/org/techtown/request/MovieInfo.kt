cd1e1ecbb88b89f27f81f205fa9d73d0459750d4
jongmin <ace4463@gmail.com>
2022-05-23 14:23:21 +0900

    Add MyRequset

---
+++ MyRequest/app/src/main/java/org/techtown/request/MovieInfo.kt
@@ -0,0 +1,13 @@
+package org.techtown.request
+
+data class MovieInfo(
+    val rank: String?,
+    val rankInten: String?,
+    val rankOldAndNew: String?,
+    val movieCd: String?,
+    val movieNu: String?,
+    val openDt: String?,
+    val audiCnt:String?,
+    val scrnCnt:String?,
+    val showCnt:String?
+)
